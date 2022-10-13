e21c3008d22b5564df476560c3bda2f0e3c61cf5
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2022-10-13 16:57:02 +0900

    Fix tf_transposer and add sample

--- src/tf_transposer.cpp
+++ src/tf_transposer.cpp
@@ -1,40 +1,65 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
 #include <geometry_msgs/PoseStamped.h>
 
-std::string child_frame_id;
+class TfTransposer{
+public:
 
-void cb(const geometry_msgs::PoseStamped& msg){
-  static tf::TransformBroadcaster br;
-  // transpose msg to base_link
-  //divide pos by 2
-  tf::Transform transform(
-                          tf::Quaternion(
-                                         msg.pose.orientation.x,
-                                         msg.pose.orientation.y,
-                                         msg.pose.orientation.z,
-                                         msg.pose.orientation.w
-                                         ),
-                          tf::Vector3(msg.pose.position.x,
-                                      msg.pose.position.y,
-                                      msg.pose.position.z)
-                          );
-  br.sendTransform(
-                   tf::StampedTransform(
-                                        transform,
-                                        msg.header.stamp,
-                                        msg.header.frame_id,
-                                        child_frame_id
+  void cb(const geometry_msgs::PoseStampedPtr& msg){
+    tf::StampedTransform base_to_laser, laser_to_target;
+    tf::poseMsgToTF(msg->pose, laser_to_target);
+    try {
+      tfl_.waitForTransform(base_frame_id, msg->header.frame_id,
+                            msg->header.stamp, ros::Duration(3.0));
+      tfl_.lookupTransform(base_frame_id, msg->header.frame_id,
+                           msg->header.stamp, base_to_laser);
+    }
+    catch (tf2::TransformException &e) {
+      return;
+    }
+    tf::Transform base_to_target;
+    base_to_target = base_to_laser * laser_to_target;
+    tf::Transform transform(
+                            tf::Quaternion(0,0,0,1),
+                            tf::Vector3(
+                                        base_to_target.getOrigin().getX()/2.0,
+                                        base_to_target.getOrigin().getY()/2.0,
+                                        base_to_target.getOrigin().getZ()/2.0
                                         )
-                   );
-}
+                            );
+    tfb_.sendTransform(
+                     tf::StampedTransform(
+                                          transform,
+                                          msg->header.stamp,
+                                          base_frame_id,
+                                          child_frame_id
+                                          )
+                     );
+  }
 
-int main(int argc, char** argv){
+  TfTransposer(): nh_(), pnh_("~"), tfl_(nh_) {
+    pnh_.param("base_frame_id", base_frame_id, std::string("base_link"));
+    pnh_.param("child_frame_id", child_frame_id, std::string("base_link_offset"));
+    sub_ = pnh_.subscribe("input/pose", 1, &TfTransposer::cb, this);
+  }
+
+private:
+  ros::NodeHandle nh_;
+  ros::NodeHandle pnh_;
+  tf::TransformBroadcaster tfb_;
+  tf::TransformListener tfl_;
+  ros::Subscriber sub_;
+
+  std::string base_frame_id;
+  std::string child_frame_id;
+};
+
+int main(int argc, char** argv)
+{
   ros::init(argc, argv, "tf_transposer");
-  ros::NodeHandle nh("~");
-  nh.param("child_frame_id", child_frame_id, std::string("base_link_offset"));
-  ros::Subscriber sub = nh.subscribe("input/pose", 1, &cb);
-
+  TfTransposer tft;
   ros::spin();
+
   return 0;
 };
